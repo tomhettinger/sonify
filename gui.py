13434b411a62164696cf2c80dd64b20c0555d6ba
tomhettinger <ssjsonic1@gmail.com>
2013-03-09 20:27:40 -0500

    fixes including play button

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 from Tkinter import *
 from PIL import Image, ImageTk
+import os
 
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import tkSnack as snack
 
 import sonify
 import wavebender as wb
@@ -17,15 +19,16 @@ class GUI:
         # Instance Attributes
         self.targetImage = None
         self.samples = None
+        self.snd = snack.Sound()
         self.maxWidth = 1000
         self.maxHeight = 600
-        self.rate = 8000
+        self.rate = 16000
         self.time = 4
         self.myParent = parent
         self.introText = """
         Hello, and welcome to Sonify.  To begin, load an image from your
-        computer and click 'sonify'. Similar colors will be grouped into
-        notes from the C Major scale.  The color the note has, the stronger
+        computer. Similar colors will be grouped into notes from the 
+        C Major scale.  The more color a note has, the stronger
         the note is played.
 
         After you sonify, you can play it back or write it to a file.
@@ -49,24 +52,24 @@ class GUI:
         self.greetingLabel.grid(column=0, row=0, rowspan=5, columnspan=5)
         # Data frame
         self.dummyFrame2 = Frame(self.dataFrame, width=self.maxWidth/2, height=self.maxHeight/2, relief='sunken', borderwidth=2)
-        self.dummyFrame2.grid(column=0, row=0, columnspan=2, rowspan=6)
+        self.dummyFrame2.grid(column=0, row=0, columnspan=2, rowspan=5)
         # Load entry and button
-        self.loadFrame = Frame(self.dataFrame)
-        self.loadFrame.grid(column=0, row=0)
-        self.loadEntry = Entry(self.loadFrame, width=45)
-        self.loadEntry.insert(0, "./images/forest.jpg")
-        self.loadEntry.grid(column=0, row=0)
-        self.loadButton = Button(self.loadFrame, text='Load', command=self.load_image)
-        self.loadButton.grid(column=1, row=0)
+        self.loadImageFrame = Frame(self.dataFrame)
+        self.loadImageFrame.grid(column=0, row=0)
+        self.loadImageEntry = Entry(self.loadImageFrame, width=45)
+        self.loadImageEntry.insert(0, "./images/forest.jpg")
+        self.loadImageEntry.grid(column=0, row=0)
+        self.loadImageButton = Button(self.loadImageFrame, text='Load', command=self.load_image)
+        self.loadImageButton.grid(column=1, row=0)
         # Sonify button
-        self.sonifyButton = Button(self.dataFrame, text='Sonify', width=50, command=self.sonify_image)
-        self.sonifyButton.grid(column=0, row=1)
+        #self.sonifyButton = Button(self.dataFrame, text='Sonify', width=50, command=self.sonify_image)
+        #self.sonifyButton.grid(column=0, row=1)
         # Play button
         self.playButton = Button(self.dataFrame, text='Play', width=50, command=self.play_wave)
-        self.playButton.grid(column=0, row=2)
+        self.playButton.grid(column=0, row=1)
         # Write field and button
         self.writeFrame = Frame(self.dataFrame)
-        self.writeFrame.grid(column=0, row=3)
+        self.writeFrame.grid(column=0, row=2)
         self.writeEntry = Entry(self.writeFrame, width=45)
         self.writeEntry.insert(0, "./sounds/temp.wav")
         self.writeEntry.grid(column=0, row=0)
@@ -74,10 +77,10 @@ class GUI:
         self.writeButton.grid(column=1, row=0)
         # Quit button
         self.quitButton = Button(self.dataFrame, text='Quit', width=50, command=parent.destroy)
-        self.quitButton.grid(column=0, row=4)
+        self.quitButton.grid(column=0, row=3)
         # Message Label
         self.messageLabel = Label(self.dataFrame, justify="left")
-        self.messageLabel.grid(column=0, row=5)
+        self.messageLabel.grid(column=0, row=4)
         # Image frame
         self.dummyFrame3 = Frame(self.imageFrame, width=self.maxWidth/2, height=self.maxHeight/2, relief='sunken', borderwidth=2)
         self.dummyFrame3.grid(column=0, row=0)
@@ -92,6 +95,10 @@ class GUI:
         self.powerWidget = self.powerFigureCanvas.get_tk_widget()
         self.powerWidget.grid(column=0, row=1)
     
+    def __del__(self):
+        if os.path.exists('./temp.wav'):
+            os.remove('./temp.wav')
+
 
     def write_wave(self):
         self.messageLabel.configure(text="")
@@ -102,6 +109,8 @@ class GUI:
         if filename is None:
             self.messageLabel.configure(text="Enter a filename to write.")
             return
+        self.messageLabel.configure(text="Writing ...")
+        self.myParent.update_idletasks()
         try:
             wb.write_wavefile(filename, samples=self.samples, nframes=self.rate*self.time, 
                               nchannels=1, framerate=self.rate)
@@ -110,8 +119,18 @@ class GUI:
             self.messageLabel.configure(text="Write failed. Try another image or output filename.")
 
     def play_wave(self):
+        if not os.path.exists('./temp.wav'):
+            self.messageLabel.configure(text="Unable to find sonification.")
+            return
         self.messageLabel.configure(text="")
-        self.messageLabel.configure(text="Not yet implemented.")
+        self.messageLabel.configure(text="Playing ...")
+        self.myParent.update_idletasks()
+        self.snd.play(blocking=True)
+        self.messageLabel.configure(text="")
+        # Graphics
+        #c = SnackCanvas() 
+        #c.pack() 
+        #c.create_waveform(0, 0, sound=snd)   or spectrograph or powerspectrum
 
     def sonify_image(self):
         self.messageLabel.configure(text="")
@@ -129,11 +148,17 @@ class GUI:
         self.powerFigureCanvas.show()
         channels = ((sonify.super_sine_wave(freqs=sonify.tones, amps=amps, framerate=self.rate),),)
         self.samples = wb.compute_samples(channels, nsamples=self.rate*self.time)
-        self.messageLabel.configure(text="Sonification complete.")
+        try:
+            wb.write_wavefile('./temp.wav', samples=self.samples, nframes=self.rate*self.time, 
+                              nchannels=1, framerate=self.rate)
+            self.snd.read('./temp.wav')
+            self.messageLabel.configure(text="Sonification complete.")
+        except:
+            self.messageLabel.configure(text="Errors in temp write.")
 
     def load_image(self):
         self.messageLabel.configure(text="")
-        filename = self.loadEntry.get()
+        filename = self.loadImageEntry.get()
         try:
             self.targetImage = Image.open(filename)
         except:
@@ -149,12 +174,17 @@ class GUI:
         photo = ImageTk.PhotoImage(resizedImage)
         self.displayIm.configure(image=photo)
         self.displayIm.image = photo
-
+        self.sonify_image()
 
 def main():
     # Begin gui framework
+    if os.path.exists('./temp.wav'):
+        print './temp.wav already exists'
+        sys.exit()
+
     root = Tk()
     root.title("Sonify")
+    snack.initializeSnack(root)
     myGUI = GUI(root)
     root.resizable(False, False)
     root.mainloop()
