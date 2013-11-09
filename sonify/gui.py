6363c1f0e6250c649d67a6f6b5d170bee5e7d56f
tomhettinger <ssjsonic1@gmail.com>
2013-11-09 14:05:22 -0500

    fixing install again

--- sonify/gui.py
+++ sonify/gui.py
@@ -10,7 +10,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-import tkSnack as snack
+from tkSnack import tkSnack as snack
 from PIL import Image, ImageTk
 import wavebender
 
